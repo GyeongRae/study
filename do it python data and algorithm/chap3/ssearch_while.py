5c5e19a2d119e37e45868cd76f17d3350af02f5c
GyeongRae <67531670+GyeongRae@users.noreply.github.com>
2021-01-17 23:18:15 +0900

    Create ssearch_while.py

---
+++ do it python data and algorithm/chap3/ssearch_while.py
@@ -0,0 +1,28 @@
+from typing import Any, Sequence
+
+def seq_search(a: Sequence, key : Any) -> int:
+    i = 0
+    
+    while True: #배열 내 모든 원소를 대상으로 검색 실행 단 결과값 나올시 그대로 반환
+        if i == len(a):
+            return 1
+        if a[i] == key:
+            return i
+        i += 1
+        
+if __name__ == '__main__':
+    num = int(input('원소 수를 입력하세요.: '))
+    x = [None] * num # 배열 생성
+    
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: ')) #배열 값 채우기
+        
+    ky = int(input('검색할 값을 입력하세요.: '))  
+    
+    idx = seq_search(x, ky)
+    
+    if idx == -1:
+        print('검색값을 갖는 원소가 존재하지 않습니다. ')
+        
+    else:
+        print(f'검색값은 x[{idx}]에 있습니다. ')
