459be5846c80327e3e15b0d34daa02c02ef15ae0
GyeongRae <67531670+GyeongRae@users.noreply.github.com>
2021-01-18 23:09:31 +0900

    Add files via upload

---
+++ do it python data and algorithm/chap3/ssearch_test1.py
@@ -0,0 +1,22 @@
+#다양한 시퀀스에서 검ㅅ실수 검색
+from ssearch_while import seq_search
+
+print('실수를 검색합니다. ')
+print('주의: "END"를 입력하면 종료합니다.')
+
+number = 0
+x = []
+
+while True:
+    s = input(f'x[{number}]: ')
+    if s == 'End':
+        break
+    x.append(float(s)) #배열 끝에 원소 추가
+    number += 1
+
+ky = float(input('검색할 값을 입력하세요.: '))
+idx = seq_search(x, ky)
+if idx == -1:
+    print('검색값을 갖는 원소가 존재하지 않습니다. ')
+else:
+    print(f'검색값은 x[{idx}]에 있습니다.')
